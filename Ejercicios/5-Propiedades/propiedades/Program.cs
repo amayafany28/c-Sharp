8ad350e303221ba08520ddb1f37d1d5cab2750ad
Fany Amaya <amaya.fany@unah.hn>
2021-07-12 22:22:48 -0600

    Subir tarea propiedades

---
+++ Ejercicios/5-Propiedades/propiedades/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace propiedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno();
+            a.Id = 1;
+            a.PrimerNombre = "Fany";
+            a.SegundoNombre = "Amaya";
+            
+            Alumno b = new Alumno();
+            b.Id = 2;
+            b.PrimerNombre = "Yamileth";
+            b.SegundoNombre = "Amaya";
+            
+            Alumno c = new Alumno(3);
+            c.PrimerNombre = "Maria";
+
+            Alumno d = new Alumno("Patricia", "Vasquez");
+            Console.WriteLine(a.Id);
+            Console.WriteLine(b.Id);
+            Console.WriteLine(c.Id);
+            Console.WriteLine(d.PrimerNombre + " " + d.SegundoNombre);
+        }
+    }
+}
